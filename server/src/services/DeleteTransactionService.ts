7e01d7d94a63fedadabd368917e26f6c285aa207
weslybrandao <wesly.souza@jussi.com.br>
2020-06-16 19:16:08 -0300

    Adding back-end

---
+++ server/src/services/DeleteTransactionService.ts
@@ -0,0 +1,20 @@
+import { getCustomRepository, TransactionRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transactionsrepository from '../repositories/TransactionsRepository';
+
+class DeleteTransactionService {
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(Transactionsrepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionsRepository.remove(transaction);
+  }
+}
+
+export default DeleteTransactionService;
